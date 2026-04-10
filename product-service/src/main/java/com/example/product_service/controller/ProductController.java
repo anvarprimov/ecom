fa0eb55c41b30dbdefc0073f54c22cbc528cb895
anvarprimov <anvarprimov07@gmail.com>
2026-04-10 12:26:19 -0400

    test commit

--- product-service/src/main/java/com/example/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/example/product_service/controller/ProductController.java
@@ -1,15 +1,15 @@
 package com.example.product_service.controller;
 
 import com.example.product_service.dto.ProductRequestDto;
+import com.example.product_service.dto.ProductResponseDto;
+import com.example.product_service.dto.Response;
 import com.example.product_service.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,7 +19,35 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public HttpEntity<?> create(@Valid @RequestBody ProductRequestDto requestDto) {
-        return ResponseEntity.ok().build();
+    public HttpEntity<Response<ProductResponseDto>> create(@Valid @RequestBody ProductRequestDto requestDto) {
+        Response<ProductResponseDto> response = productService.create(requestDto);
+        return ResponseEntity.status(response.isSuccess()? 201 : 400).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public HttpEntity<Response<ProductResponseDto>> getOne(@PathVariable Long id) {
+        Response<ProductResponseDto> response = productService.getOne(id);
+        return ResponseEntity.status(response.isSuccess()? 200 : 404).body(response);
+    }
+
+    @GetMapping()
+    public Page<ProductResponseDto> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sort
+    ) {
+        return productService.getAll(page, size, sort);
+    }
+
+    @PutMapping("/{id}")
+    public HttpEntity<Response<ProductResponseDto>> update(@PathVariable Long id, @Valid @RequestBody ProductRequestDto requestDto) {
+        Response<ProductResponseDto> response = productService.update(id, requestDto);
+        return ResponseEntity.status(response.isSuccess()? 200 : 409).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpEntity<Response<Object>> softDelete(@PathVariable Long id) {
+        Response<Object> response = productService.softDelete(id);
+        return ResponseEntity.status(response.isSuccess()? 204 : 409).body(response);
     }
 }
--- product-service/src/main/java/com/example/product_service/service/ProductService.java
+++ product-service/src/main/java/com/example/product_service/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.example.product_service.service;
 
+import com.example.product_service.dto.ProductRequestDto;
+import com.example.product_service.dto.ProductResponseDto;
+import com.example.product_service.dto.Response;
 import com.example.product_service.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +13,23 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    public Response<ProductResponseDto> create(ProductRequestDto requestDto) {
+        return null;
+    }
+
+    public Response<ProductResponseDto> getOne(Long id) {
+        return null;
+    }
+
+    public Page<ProductResponseDto> getAll(int page, int size, String sort) {
+        return null;
+    }
+
+    public Response<ProductResponseDto> update(Long id, ProductRequestDto requestDto) {
+        return null;
+    }
+
+    public Response<Object> softDelete(Long id) {
+        return null;
+    }
 }
