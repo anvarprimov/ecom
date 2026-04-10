7de29de48862facf095f4def7b029620feedd543
anvarprimov <anvarprimov07@gmail.com>
2026-04-10 17:28:15 -0400

    api gateway up running

---
+++ api-gateway/src/main/java/com/example/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package com.example.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("eureka", r -> r
+                        .path("/eureka", "/eureka/")
+                        .filters(f -> f.rewritePath("/eureka", "/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-static", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+
+                .route("product-service", r -> r
+                        .path("/api/products", "/api/products/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+                .build();
+    }
+}
